fcf23c7b728db1ad28b28e41c75a6f7f924e8f42
Kisor-S <kishorevk910@gmail.com>
2024-05-06 13:48:54 +0530

    Create button for Random color while drwing the squares

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
-const gridArea = 600;
+const gridArea = 700;
 let boxesPerSide = 16;
+let mouseIsDown = false;
+let blk = true;
+let ran = true;
+let ers = true;
 
 const sketchArea = document.querySelector("#sketch-area");
 sketchArea.style.width = sketchArea.style.height = `${gridArea}px` ;
+const randm = document.querySelector("#randm");
 
 newGrid.addEventListener("click", () => {
     let gridSize = prompt("Please enter the no.of squares for each side");
@@ -12,51 +17,87 @@ newGrid.addEventListener("click", () => {
 })
 
 
-function setBackgroundColor(){
-    this.style.backgroundColor = "black";
+function setBackgroundColorBlack(){
+    if(blk){
+        if(mouseIsDown){
+            this.style.backgroundColor = "black";
+        }
+    }
 }
 
-function createGrids() {
+function eraseBackgroundColor() {
+    if(ers){
+        if(mouseIsDown){
+            this.style.backgroundColor = "white";
+        }
+    }
+}
 
-    sketchArea.textContent="";
-    const totalSquares = `${boxesPerSide * boxesPerSide}`;
-    const widthOrHeight = `${(gridArea / boxesPerSide)}px`;
-
-    for(let i =0 ; i < totalSquares; i++)
-    {
-        const gridCells = document.createElement("div");
-        gridCells.style.width = gridCells.style.height = widthOrHeight;
-
-        sketchArea.appendChild(gridCells).className = "cell";
-
-        gridCells.addEventListener("mouseover", setBackgroundColor);
-
-        erase.addEventListener("click" , () => {
-            gridCells.addEventListener("mouseover", () => {
-                gridCells.style.backgroundColor = "white";
-            })
-        })
-
-        black.addEventListener("click" , () => {
-            gridCells.addEventListener("mouseover", () => {
-                gridCells.style.backgroundColor = "black";
-            })
-        })
-
-
-        clear.addEventListener("click", () => {
-            gridCells.style.backgroundColor = "White";
-        })
-
-
-        randm.addEventListener("click", () => {
-            gridCells.addEventListener("mouseover")
-        })
+const hex = '0123456789ABCDEF';
+function setBackgroundRandColor () {
+    if(ran){
+        if(mouseIsDown){
+            let color = '#';
+            for (let i = 0 ; i < 6 ; i++){
+                color = color + hex[Math.floor(Math.random() * 16)];
+            }
+            this.style.backgroundColor = color
+        } ;
     }
 }
 
 
+function createGrids() {
 
+    if(boxesPerSide < 2 || boxesPerSide > 100){
+        alert("Please enter a number between 2 and 100");
+    }
+
+    else {
+
+        sketchArea.textContent="";
+        const totalSquares = `${boxesPerSide * boxesPerSide}`;
+        const widthOrHeight = `${(gridArea / boxesPerSide)}px`;
+
+        for(let i =0 ; i < totalSquares; i++)
+        {
+            const gridCells = document.createElement("div");
+            gridCells.style.width = gridCells.style.height = widthOrHeight;
+
+            sketchArea.appendChild(gridCells).className = "cell";
+
+            gridCells.addEventListener("mouseenter", setBackgroundColorBlack);
+            gridCells.addEventListener("mousedown", function(){mouseIsDown = true});
+            gridCells.addEventListener("mouseup", function(){mouseIsDown = false});
+
+            erase.addEventListener("click" , () => {
+                blk = false;
+                ran = false;
+                ers = true;
+                gridCells.addEventListener("mouseover", eraseBackgroundColor);
+            });
+
+            black.addEventListener("click" , () => {
+                ran = false;
+                ers = false;
+                blk = true ;
+                gridCells.addEventListener("mouseover", setBackgroundColorBlack);
+            });
+            
+            clear.addEventListener("click", () => {
+                gridCells.style.backgroundColor = "White";
+            })
+
+
+            randm.addEventListener("click", () => {
+                ran = true;
+                ers = false;
+                blk = false;
+                gridCells.addEventListener("mouseover", setBackgroundRandColor);
+            });
+        }
+    }
+}
 
 createGrids();
 
